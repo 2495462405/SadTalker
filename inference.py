9f075ac884ef7acda0afbc515b1692cc043bbf37
cv <zjj.hit.sz@gmail.com>
2023-06-11 14:19:59 +0800

    b1

--- inference.py
+++ inference.py
@@ -28,6 +28,8 @@ def main(args):
     ref_eyeblink = args.ref_eyeblink
     ref_pose = args.ref_pose
 
+    #wasadad
+
     current_root_path = os.path.split(sys.argv[0])[0]
 
     sadtalker_paths = init_path(args.checkpoint_dir, os.path.join(current_root_path, 'src/config'), args.size, args.old_version, args.preprocess)
